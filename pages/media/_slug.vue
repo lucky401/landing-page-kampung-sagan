aca01badf1d31aab9cfb5d040bf0c1846fa202df
Lucky DS <luckydewa123@gmail.com>
2023-01-29 17:34:09 +0700

    feat: add media page

---
+++ pages/media/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-lazy>
+    <v-container>
+      <v-row>
+        <v-col class="my-3" cols="12">
+          <nuxt-content :document="blog" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-lazy>
+</template>
+<script>
+export default {
+  layout: 'content',
+
+  async asyncData({ $content, params }) {
+    const blog = await $content(`articles/${params.slug}`).fetch();
+    return {
+      blog,
+    };
+  },
+};
+</script>
---
+++ pages/media/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-lazy>
+    <v-container>
+      <v-row class="mx-0">
+        <v-col>
+          <h1>Artikel Kampung Sagan</h1>
+          <p class="mt-2">
+            Artikel - artikel yang berhubungan dengan Kampung Sagan Yogyakarta
+          </p>
+        </v-col>
+      </v-row>
+      <v-row class="mx-0">
+        <v-col v-for="article in articles" :key="article.slug" cols="12" md="4">
+          <v-card class="elevation-12" :to="`/media/${article.slug}`">
+            <v-img :src="article.image"></v-img>
+            <v-card-title>{{ article.title }}</v-card-title>
+            <v-card-text>{{ article.description }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-lazy>
+</template>
+<script>
+export default {
+  layout: 'content',
+
+  async asyncData({ $content }) {
+    const articles = await $content('articles').fetch();
+    return {
+      articles,
+    };
+  },
+};
+</script>
